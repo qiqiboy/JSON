01c5090efcbad91e8f83ca7e9b389c3f63bd0de6
qiqiboy <imqiqiboy@gmail.com>
2013-07-24 19:27:29 +0800

    add js

---
+++ JSON.js
@@ -0,0 +1,51 @@
+window.JSON=window.JSON || {
+	/**
+	 * 将变量转为json字串
+	 */
+	stringify: function(obj){
+		try{
+			if('toJSON' in obj){
+				return '"'+obj.toJSON()+'"';
+			}
+		}catch(e){}
+		
+		var isArr=false;
+		switch(type(obj)){
+			case 'number':
+			case 'boolean':
+				return isNaN(obj)?'null':''+obj;
+			case 'date':
+			case 'string':
+				return '"'+obj+'"';
+			case 'regexp':
+				return '{}';
+			case 'array':isArr=true;
+			case 'object':
+				var carr=[],
+					str=isArr?'[':'{',
+					n;
+				if(isArr){
+					for(n=0;n<obj.length;n++){
+						carr.push(arguments.callee(obj[n]));
+					}
+				}else{
+					for(n in obj){
+						carr.push('"'+n+'":'+arguments.callee(obj[n]));
+					}
+				}
+				return str+=carr.join(',')+(isArr?']':'}');
+			default:return 'null';
+		}
+
+		function type(obj){
+			var mt=obj==null?[0,String(obj)]:/\[object (\w+)\]/i.exec(Object.prototype.toString.call(obj));
+			return mt[1]?mt[1].toLowerCase():'object';
+		}
+	},
+	/**
+	 * 解析json字串
+	 */
+	parse:function(str){
+		return eval("("+str+")");
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 JSON
 ====
 
-解析/反解析json字符串的javascript组件
+为不支持JSON解析的早期浏览器提供解析/反解析json字符串的javascript组件
+
+stringify方法支持深度转换
+
+## 使用方法
+```javascript
+//直接按照标准的JSON操作来就行
+
+JSON.parse(str); //解析json字串
+
+JSON.stringify(obj); //将变量转为json字串
+````
